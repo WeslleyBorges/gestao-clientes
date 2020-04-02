1016d65472658089a3423d191d3b41d7fcd73b42
Weslley <weslleynfs@gmail.com>
2020-04-02 05:50:46 -0400

    commit emergencial

--- vendas/actions.py
+++ vendas/actions.py
@@ -1,10 +1,13 @@
 def mudar_nfe_emitida(modeladmin, request, queryset):
-  if request.user.has_perm('vendas.setar_nfe')
+  if request.user.has_perm('vendas.setar_nfe'):
     queryset.update(nfe_emitida=True)
-    
+
+
 mudar_nfe_emitida.short_description = 'NF-e emitida'
+
 
 def mudar_nfe_nao_emitida(modeladmin, request, queryset):
   queryset.update(nfe_emitida=False)
+
 
 mudar_nfe_nao_emitida.short_description = 'NF-e não emitida'
